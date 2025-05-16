c4c50d4dba31c73b90d7d4d865e31ddf600a8d5a
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-16 22:23:02 +0530

    Add files via upload

---
+++ program3b4.java
@@ -0,0 +1,33 @@
+/*Write a Java Program for Eliminating all whitespace characters from a string using user defined
+function removeWhitespace()*/
+package githublab;
+import java.util.Scanner;
+public class program3b4 {
+	
+
+
+	    // User-defined function to remove all whitespace characters
+	    public static String removeWhitespace(String str) {
+	        if (str == null) {
+	            return null;
+	        }
+	        // Use regular expression to replace all whitespace characters
+	        return str.replaceAll("\\s+", "");
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        // Input from user
+	        System.out.print("Enter a string with whitespace: ");
+	        String input = sc.nextLine();
+
+	        // Remove whitespace and display result
+	        String result = removeWhitespace(input);
+	        System.out.println("String without whitespace: " + result);
+
+	        sc.close();
+	    }
+	}
+
+
