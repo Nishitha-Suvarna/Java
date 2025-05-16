c4d1aff59aebef95f4f341a62fc2726122983b9a
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-16 22:36:40 +0530

    Add files via upload

---
+++ program3b5.java
@@ -0,0 +1,37 @@
+/*Write a Java Program for Reversing the characters in a string using user defined function
+reverseString().*/
+package githublab;
+import java.util.Scanner;
+public class program3b5 {
+	
+
+	    // User-defined function to reverse a string
+	    public static String reverseString(String str) {
+	        if (str == null) {
+	            return null;
+	        }
+
+	        String reversed = "";
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            reversed += str.charAt(i); // Concatenate characters in reverse order
+	        }
+
+	        return reversed;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        // Input
+	        System.out.print("Enter a string to reverse: ");
+	        String input = sc.nextLine();
+
+	        // Call the reverse function and print result
+	        String result = reverseString(input);
+	        System.out.println("Reversed string: " + result);
+
+	        sc.close();
+	    }
+	}
+
+
