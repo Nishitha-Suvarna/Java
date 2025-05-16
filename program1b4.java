bb1a5a0417d2c011619593cfb44a88747d089431
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-16 19:38:53 +0530

    Add files via upload

---
+++ program1b4.java
@@ -0,0 +1,15 @@
+package githublab;
+import java.util.*;
+public class program1b4 {
+	public static void main(String[] args) {
+	List<String> ls = new ArrayList<>();
+	ls.add("Orange");
+	ls.add("geen");
+	ls.add("pink");
+	ls.add("red");
+	ls.add("Blue");
+	System.out.println(ls);
+	System.out.println("using sublist:");
+	System.out.println(ls.subList(0,2));
+}
+}
