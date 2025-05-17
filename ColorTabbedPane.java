daf871de1320c9c3995e98bc4b6cdccbffc55d97
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-17 19:06:04 +0530

    Add files via upload

---
+++ ColorTabbedPane.java
@@ -0,0 +1,41 @@
+package githublab;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ColorTabbedPane extends JFrame {
+
+    public ColorTabbedPane() {
+        setTitle("Color Tabbed Pane");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create JTabbedPane
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        // Create panels for each color
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.RED);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.BLUE);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.GREEN);
+
+        // Add tabs to the tabbed pane
+        tabbedPane.addTab("RED", redPanel);
+        tabbedPane.addTab("BLUE", bluePanel);
+        tabbedPane.addTab("GREEN", greenPanel);
+
+        // Add tabbed pane to frame
+        add(tabbedPane);
+
+        setLocationRelativeTo(null); // Center the window
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ColorTabbedPane();
+    }
+}
