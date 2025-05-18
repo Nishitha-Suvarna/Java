08056c5409b4e7343145a1a0570cf691bf3de3b8
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-18 11:33:37 +0530

    ExtractColors.java

--- ExtractColors.java
+++ ExtractColors.java
@@ -5,12 +5,15 @@ import java.util.*;
 public class program1b4 {
 	public static void main(String[] args) {
 	List<String> ls = new ArrayList<>();
+		
+        // Add colors to the list
 	ls.add("Orange");
 	ls.add("geen");
 	ls.add("pink");
 	ls.add("red");
 	ls.add("Blue");
 	System.out.println(ls);
+	// Extract 1st and 2nd elements (index 0 and 1)
 	System.out.println("using sublist:");
 	System.out.println(ls.subList(0,2));
 }
