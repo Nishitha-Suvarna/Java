583b0c27cf5795cba085526ada7cbc19ad82abb3
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-18 11:46:18 +0530

    IterateFromSecond.java

--- IterateFromSecond.java
+++ IterateFromSecond.java
@@ -6,11 +6,13 @@ public class program1c1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String>	ls = new LinkedList<>();
+		// Create a LinkedList of colors
 		ls.add("Orange");
 		ls.add("geen");
 		ls.add("pink");
 		ls.add("red");
 		ls.add("Blue");
+		// Create a ListIterator starting from index 2
 		Iterator iterator = ls.listIterator(2);
 		while (iterator.hasNext()) {
 		System.out.println(iterator.next());
