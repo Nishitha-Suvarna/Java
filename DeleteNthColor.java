ea7900f92d8ccd2d7af9ef922277ae8e72f12f05
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-18 11:42:10 +0530

    DeleteNthColor.java

--- DeleteNthColor.java
+++ DeleteNthColor.java
@@ -7,15 +7,15 @@ public class program1b5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> ls = new ArrayList<>();
+		// Add colors to the list
 		ls.add("Orange");
 		ls.add("green");
 		ls.add("pink");
 		ls.add("red");
 		ls.add("Blue");
-//		int n=3;
-		String Str="green";
+		int n=1;
 		System.out.println(ls);
-		System.out.println("Removing the nth Element:"+ls.remove(Str));
+		System.out.println("Removing the nth Element:"+ls.remove(n));
 		System.out.println(ls);
 	}
 
