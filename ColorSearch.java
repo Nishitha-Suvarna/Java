de14e40da28774ff2ad4dd26d8b13d0e951aa7eb
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-18 11:22:59 +0530

    ColorSearch.java

--- ColorSearch.java
+++ ColorSearch.java
@@ -6,11 +6,13 @@ public class p1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> ls = new ArrayList<>();
+		// Add colors to the ArrayList
 		ls.add("Orange");
 		ls.add("geen");
 		ls.add("pink");
 		ls.add("red");
 		System.out.println(ls);
+		// Check if "Red" is present
 		if(ls.contains("red")) {
 			System.out.println("available");
 		}else {
