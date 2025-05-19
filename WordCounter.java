ba51fb7bf0a29b11713c69f808c33664fd77e55d
Nishitha-Suvarna <nishithasuvarna74@gmail.com>
2025-05-19 20:48:27 +0530

    Update WordCounter.java

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,5 @@
+/*Write a Java Program for Counting the number of words in a string using user defined function
+countWords()*/
 package githublab;
 import java.util.Scanner;
 public class WordCounter {
